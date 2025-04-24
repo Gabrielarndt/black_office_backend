7b4e98c19bf0332f67038340afb238f3ef66182e
Gabrielarndt <gabrielharndt@gmail.com>
2025-04-24 14:00:59 -0300

    up

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5500', 'steelblue-whale-123218.hostingersite.com', 'https://black-office-site-1.onrender.com/'],
+  origin: ['http://localhost:5500', 'steelblue-whale-123218.hostingersite.com', 'http://127.0.0.1:5500'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
